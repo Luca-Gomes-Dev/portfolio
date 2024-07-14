909b392cfc75ada38e0c33c2c354bd88eda908cd
Luca-Gomes-Dev <lcgvasconcelos1@gmail.com>
2024-07-14 09:07:25 -0300

    Responsividade tela menor 320px
    Responsividade tela menor 320px 2 comit

--- src/pages/Page404/index.jsx
+++ src/pages/Page404/index.jsx
@@ -1,19 +1,15 @@
-import Header from '../../components/Header'
-import Footer from '../../components/Footer'
-import Container from '../../components/Container'
 import styles from './Page404.module.css'
-export default function Page404() {
-    return(
-        <>
-            <Header />
-            <Container>
-                <h2 className={styles.titulo2}>Algo de errado não está certo!</h2>
-                <div className={styles.textos}>
-                    <span className={styles.texto_grande}>404</span> <br/>
-                    <strong className={styles.texto_vermelho}>Página não Localizada!</strong>
-                </div>
-            </Container>
-            <Footer />
-        </>
+
+function Page404() {
+    return (
+        <section className={styles.page404}>
+            <h2 className={styles.titulo2}>Algo de errado não está certo!</h2>
+            <div className={styles.textos}>
+                <span className={styles.texto_grande}>404</span> <br />
+                <strong className={styles.texto_vermelho}>Página Não Localizada!</strong>
+            </div>
+        </section>
     )
 }
+
+export default Page404
